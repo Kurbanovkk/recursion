405dae52816ed11b3ca2da4614a58334b99011e7
Kurban <kurbanovkk96@mail.ru>
2023-09-17 17:03:25 +0300

    Добавьте файлы проекта.

---
+++ Fibonacci.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    internal class Fibonacci
+    {
+        public int ParseInput(string input)
+        {
+            if (!int.TryParse(input, out int value))
+            {
+                throw new FormatException($"Значение '{input}' не является целым числом.");
+            }
+            return value;
+        }
+
+        public int Fib(int n)
+        {
+            if (n < 0) throw new ArgumentException("n должно быть положительным целым числом.");
+
+            if (n == 0 || n == 1) return n;
+
+
+            return (Fib(n - 1) + Fib(n - 2));
+        }
+
+        public void PrintFibonacci(int n)
+        {
+            Console.Write("0 ");
+            for (int i = 1; i <= n; i++)
+            {
+                int fibonacciNumber = Fib(i);
+                Console.Write(fibonacciNumber + " ");
+            }
+        }
+
+        public int Fib2(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("n должно быть положительным числом.");
+            }
+            else if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                int previous = 1;
+                int current = 1;
+                int result = 0;
+
+                for (int i = 3; i <= n; i++)
+                {
+                    result = previous + current;
+                    previous = current;
+                    current = result;
+                }
+
+                return result;
+            }
+        }
+
+        public void PrintFib2(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                int fibon = Fib2(i);
+                Console.Write(fibon + " ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch iterativeStopwatch = new Stopwatch();
+            var fibonacci = new Fibonacci();
+            Console.WriteLine("Введите число n, нахождения n-го члена последовательности Фибоначчи");
+            var value = Console.ReadLine();
+            var n = fibonacci.ParseInput(value);
+            fibonacci.PrintFibonacci(n);
+
+            Console.WriteLine();
+            Console.WriteLine("*****************************");
+            Console.WriteLine();
+
+            fibonacci.PrintFib2(n);
+            Console.WriteLine();
+
+            iterativeStopwatch.Start();
+            fibonacci.Fib(n);
+            iterativeStopwatch.Stop();
+            Console.WriteLine($"рекурсия отработала за {iterativeStopwatch.ElapsedMilliseconds} ms, {iterativeStopwatch.ElapsedTicks} ticks");
+            iterativeStopwatch.Reset();
+
+            iterativeStopwatch.Start();
+            fibonacci.Fib2(n);
+            iterativeStopwatch.Stop();
+            Console.WriteLine($"цикл отработал за {iterativeStopwatch.ElapsedMilliseconds} ms, {iterativeStopwatch.ElapsedTicks} ticks");
+
+        }
+    }
+}
